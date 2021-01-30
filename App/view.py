e5ce2d0d166917cde8376031863dcb808cdcb0a1
JumaKnav <jm.rodriguezc1@uniandes.edu.co>
2021-01-30 18:23:31 -0500

    Se agregaron lineas de código a view.py para que el usuario pueda elegir una tercera opción para cargar los libros con sus respectivas tags.

--- App/view.py
+++ App/view.py
@@ -43,6 +43,7 @@ def printMenu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TO-DO: Modificaciones para completar el laboratorio 1.
+    print("3- Cargar los Libros con las Tags asignadas.")
     print("0- Salir")
 
 
@@ -58,6 +59,10 @@ def loadTags():
     Carga los Tags
     """
     return controller.loadTags('GoodReads/tags.csv')
+
+
+def loadBooksTags():
+    return controller.loadBooksTags('GoodReads/book_tags-small.csv')
 
 
 """
@@ -76,6 +81,10 @@ while True:
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
     # TO-DO: Modificaciones para completar el laboratorio 1.
+    elif int(inputs[0]) == 3:
+        print("Cargando información de los libros con sus tags....")
+        book_tags = loadBooksTags()
+        print('Total de libros con tags cargados: ' + str(lt.size(book_tags)))
 
     else:
         sys.exit(0)
